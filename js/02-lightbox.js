002ad76e1b58fff34375bcfd3faff6820b20a7e2
Black Chaos <black.chaos.no1@gmail.com>
2023-07-22 19:58:21 +0300

    feat: add task #2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+// gallery.addEventListener('click', )
+
+gallery.insertAdjacentHTML('afterbegin', murkupGalery(galleryItems));
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionSelector: 'img',
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: '250',
+});
+
+function murkupGalery(arr) {
+    return arr
+        .map(
+            ({ original, preview, description }) =>
+                `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`
+        )
+        .join('');
+}
