cc7214760f030d1171dc51e528548b33341b9a49
Black Chaos <black.chaos.no1@gmail.com>
2023-07-22 17:44:25 +0300

    feat: add other variant task #1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -10,20 +10,42 @@ function openLargeImg(e) {
     if (e.target.nodeName !== 'IMG') return;
 
     const instance = basicLightbox.create(
-        `<img width="1400" height="900" src="${e.target.dataset.source}">`
+        `<img width="1400" height="900" src="${e.target.dataset.source}">`, {
+            onShow: instance => document.addEventListener('keydown', closeKey),
+            onClose: instance => document.removeEventListener('keydown', closeKey),
+    }
     );
 
     instance.show();
 
-    document.addEventListener('keydown', closeKey);
-
     function closeKey(e) {
         if (e.code === 'Escape') {
             instance.close();
-            document.removeEventListener('keydown', closeKey);
         }
     }
+
+    
 }
+
+// function openLargeImg(e) {
+//     e.preventDefault();
+//     if (e.target.nodeName !== 'IMG') return;
+
+//     const instance = basicLightbox.create(
+//         `<img width="1400" height="900" src="${e.target.dataset.source}">`
+//     );
+
+//     instance.show();
+
+//     document.addEventListener('keydown', closeKey);
+
+//     function closeKey(e) {
+//         if (e.code === 'Escape') {
+//             instance.close();
+//             document.removeEventListener('keydown', closeKey);
+//         }
+//     }
+// }
 
 function makeGalleryItems(arr) {
     return arr.map(i => {
