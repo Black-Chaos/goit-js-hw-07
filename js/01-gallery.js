c7d90c8611167757835596cf3352758216d7549e
Black Chaos <black.chaos.no1@gmail.com>
2023-07-22 16:25:32 +0300

    feat: add task #1

---
+++ js/01-gallery.js
@@ -0,0 +1,44 @@
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector('.gallery');
+gallery.addEventListener('click', openLargeImg);
+
+gallery.append(...makeGalleryItems(galleryItems));
+
+function openLargeImg(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') return;
+
+    const instance = basicLightbox.create(
+        `<img width="1400" height="900" src="${e.target.dataset.source}">`
+    );
+
+    instance.show();
+
+    document.addEventListener('keydown', closeKey);
+
+    function closeKey(e) {
+        if (e.code === 'Escape') {
+            instance.close();
+            document.removeEventListener('keydown', closeKey);
+        }
+    }
+}
+
+function makeGalleryItems(arr) {
+    return arr.map(i => {
+        const item = document.createElement('li');
+        item.className = 'gallery__item';
+        const a = document.createElement('a');
+        a.className = 'gallery__link';
+        a.href = 'large-image.jpg';
+        const img = document.createElement('img');
+        img.className = 'gallery__image';
+        img.src = i.preview;
+        img.dataset.source = i.original;
+        img.alt = i.description;
+        a.append(img);
+        item.append(a);
+        return item;
+    });
+}
